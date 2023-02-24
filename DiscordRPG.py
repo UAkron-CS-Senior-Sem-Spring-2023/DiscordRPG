7c99462d8e6087a6860cc6608d0eb73715bfe29d
Brandon Cox <bcc56@uakron.edu>
2023-02-24 12:49:09 -0500

    Add /help command

--- DiscordRPG.py
+++ DiscordRPG.py
@@ -1,4 +1,5 @@
 import discord
+import inspect
 import os
 from dotenv import load_dotenv
 
@@ -16,27 +17,30 @@ async def on_ready():
 
     print('We have successfully loggged in as {0.user}'.format(client))
 
-
 @client.event
 async def on_message(message):
     if message.author == client.user:
 
         return
 
-
-
     if message.content.lower() == 'hello':
         await message.channel.send(f'Hello, {message.author.display_name}!')
 
         return
-
-
 
     if message.content.lower() == 'bye':
         await message.channel.send(f'See you later, {message.author.display_name}!')
 
         return
 
-
+    if message.content.lower() == '/help':
+        commandList = f"""
+                      List of commands:
+                      /createCharacter - create your character
+                      /viewCharacter [name] - view selected character's stats and equiptment 
+                      /classList - view list of classes
+                      /classStats [className] - view stats of selected class
+                      """
+        await message.channel.send(inspect.cleandoc(commandList))
 
 client.run(TOKEN)
