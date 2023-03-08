103839b67181739d594df9883596fb4e479bd86f
Brandon Cox <bcc56@uakron.edu>
2023-03-07 21:46:37 -0500

    Add boar monster

--- monsters.py
+++ monsters.py
@@ -1,11 +1,24 @@
 class Goblin:
     #constructor
     def __init__(self, level):
-        self._name = Goblin
+        self._name = "goblin"
         self._level = level
-        self._vigor = 3 + level
-        self._str = 2 + level
-        self._dex = 4 + (level*2)
+        self._vigor = 3 + self._level
+        self._str = 2 + self._level
+        self._dex = 4 + (self._level*2)
+        self._int = 2
+        self._health = self._vigor * 4 
+
+    #methods
+
+class Boar:
+    #constructor
+    def __init__(self, level):
+        self._name = "boar"
+        self._level = level
+        self._vigor = 4 + (self._level*2)
+        self._str = 3 + (self._level*2)
+        self._dex = 2
         self._int = 1
         self._health = self._vigor * 4 
 
