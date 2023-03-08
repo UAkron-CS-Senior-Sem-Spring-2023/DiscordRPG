9d60b347dd169483e2afe9dda6b6d756b4d15218
Brandon Cox <bcc56@uakron.edu>
2023-03-07 22:08:03 -0500

    Add wolf monster

--- monsters.py
+++ monsters.py
@@ -1,16 +1,3 @@
-class Goblin:
-    #constructor
-    def __init__(self, level):
-        self._name = "goblin"
-        self._level = level
-        self._vigor = 3 + self._level
-        self._str = 2 + self._level
-        self._dex = 4 + (self._level*2)
-        self._int = 2
-        self._health = self._vigor * 4 
-
-    #methods
-
 class Boar:
     #constructor
     def __init__(self, level):
@@ -20,6 +7,32 @@ class Boar:
         self._str = 3 + (self._level*2)
         self._dex = 2
         self._int = 1
+        self._health = self._vigor * 4 
+
+    #methods
+
+class Wolf:
+    #constructor
+    def __init__(self, level):
+        self._name = "wolf"
+        self._level = level
+        self._vigor = 4 + self._level
+        self._str = 3 + (self._level*2)
+        self._dex = 2 + self._level
+        self._int = 2
+        self._health = self._vigor * 4 
+
+    #methods
+
+class Goblin:
+    #constructor
+    def __init__(self, level):
+        self._name = "goblin"
+        self._level = level
+        self._vigor = 5 + self._level
+        self._str = 3 + self._level
+        self._dex = 5 + (self._level*2)
+        self._int = 3
         self._health = self._vigor * 4 
 
     #methods
